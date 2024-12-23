d30395ea62b13ea96825d75b5b4c2c99511f0e66
jsr029 <jsr059@gmail.com>
2024-12-23 20:29:35 +0100

    j

--- projects-jsr029-backend/server.js
+++ projects-jsr029-backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
     } catch (err) {
         console.error(err.message);
