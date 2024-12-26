f8b0ebf3056926abd7a5cb2b68cf775bd8c3e050
jsr029 <jsr059@gmail.com>
2024-12-26 16:14:32 +0100

    kl

--- projects-jsr029-frontend/src/pages/ResumePage.js
+++ projects-jsr029-frontend/src/pages/ResumePage.js
@@ -18,8 +18,8 @@ const ResumePage = () => {
                     <section className="resume-section">
                         <h2>Experience</h2>
                         <div className="resume-item">
-                            <h3>Développeur d'Appications et Dev UX</h3>
-                            <p>Benomads.fr | août 2024 - Present</p>
+                            <h3>Développeur d'Appications et Dev UX (Bénévolat)</h3>
+                            <p>Pour Benomads.fr | août 2024 - Present</p>
                             <ul>
                                 <li>Créer des prototypes figma pour rendre la navigation intuitive</li>
                                 <li>Développer des micro services comme à l'inscription l'application se crée automatiquement</li>
@@ -27,8 +27,8 @@ const ResumePage = () => {
                             </ul>
                         </div>
                         <div className="resume-item">
-                            <h3>Développeur Filemaker et Filemaker Server</h3>
-                            <p>Consultant | août 2024 - octobre 2024</p>
+                            <h3>Développeur Filemaker et Filemaker Server (Bénévolat)</h3>
+                            <p>Pour un Consultant | août 2024 - octobre 2024</p>
                             <ul>
                                 <li>Créer une application Filemaker</li>
                                 <li>Utiliser les APIs de 2 sites web redmine et jira en créant mes propres APIs</li>
