9929bc03f22588eb8acac525c434a382ccc3567b
jsr029 <jsr059@gmail.com>
2024-12-26 14:39:58 +0100

    coué

--- projects-jsr029-frontend/src/components/Navbar.js
+++ projects-jsr029-frontend/src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { logout } from '../redux/actions';
 
-const Navbar = ({ setShowLogin, setShowRegister }) => {
+const CustomNavbar = ({ setShowLogin, setShowRegister }) => {
     const auth = useSelector(state => state.auth);
     const dispatch = useDispatch();
 
@@ -11,31 +12,29 @@ const Navbar = ({ setShowLogin, setShowRegister }) => {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">Mes Projets</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav ml-auto">
+        <Navbar expand={false} bg="dark" variant="dark" fixed="top" collapseOnSelect>
+            <Navbar.Brand href="/">Mes Projets</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+                <Nav className="ml-auto">
                     {!auth.isAuthenticated ? (
                         <>
-                            <li className="nav-item">
-                                <button className="btn btn-link nav-link" onClick={() => setShowLogin(true)}>se Loguer</button>
-                            </li>
-                            <li className="nav-item">
-                                <button className="btn btn-link nav-link" onClick={() => setShowRegister(true)}>s'enregistrer</button>
-                            </li>
+                            <Nav.Item>
+                                <Button variant="link" onClick={() => setShowLogin(true)}>se Loguer</Button>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Button variant="link" onClick={() => setShowRegister(true)}>s'enregistrer</Button>
+                            </Nav.Item>
                         </>
                     ) : (
-                        <li className="nav-item">
-                            <button className="btn btn-link nav-link" onClick={handleLogout}>sortir</button>
-                        </li>
+                        <Nav.Item>
+                            <Button variant="link" onClick={handleLogout}>sortir</Button>
+                        </Nav.Item>
                     )}
-                </ul>
-            </div>
-        </nav>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
-export default Navbar;
+export default CustomNavbar;
