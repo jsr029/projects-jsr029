d46d250f3849a537c8db121bde8dd71634ddce91
jsr029 <jsr059@gmail.com>
2024-12-26 13:55:06 +0100

    nav

--- projects-jsr029-frontend/src/components/Navbar.js
+++ projects-jsr029-frontend/src/components/Navbar.js
@@ -13,7 +13,10 @@ const Navbar = ({ setShowLogin, setShowRegister }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">Mes Projets</a>
-            <div className="collapse navbar-collapse">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
                     {!auth.isAuthenticated ? (
                         <>
