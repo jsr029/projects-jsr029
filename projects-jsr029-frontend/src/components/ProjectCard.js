76d9e714501bb89445722d3be809900db781d02a
jsr029 <jsr059@gmail.com>
2024-12-26 12:51:11 +0100

    c

--- projects-jsr029-frontend/src/components/ProjectCard.js
+++ projects-jsr029-frontend/src/components/ProjectCard.js
@@ -29,7 +29,7 @@ const ProjectCard = ({ project, setEditingProject, setShowProjectForm }) => {
                 }}>{project.title}</h5>
                 <p className="card-text" style={{
                     width: '120px'
-                }}>{project.description}.substring(0, 200) + '...';</p>
+                }}>{project.description.substring(0, 200)} </p>
                 <p style={{width: '390px', overflow:'hidden'}}><a href={project.appUrl} className="card-text" target="_blank" rel="noopener noreferrer" style={{
                     whiteSpace: 'nowrap',
                     overflow: 'hidden',
