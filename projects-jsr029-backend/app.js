b52c1dfdfc3a016dff5422279bf7783f1daa1e5d
jsr029 <jsr059@gmail.com>
2024-12-26 12:36:05 +0100

    nn

--- projects-jsr029-backend/app.js
+++ projects-jsr029-backend/app.js
@@ -12,6 +12,13 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express();
 
 app.use(cors());
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
