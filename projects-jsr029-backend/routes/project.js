9e7594fcfbbb08654b80c7bc9469d023f1d39f6f
jsr029 <jsr059@gmail.com>
2025-01-15 16:35:21 +0100

    yes

--- projects-jsr029-backend/routes/project.js
+++ projects-jsr029-backend/routes/project.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const Project = require('../models/project');
 const auth = require('../middleware/auth');
 const upload = require('../middleware/upload');
