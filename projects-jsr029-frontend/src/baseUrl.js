dba2f800f269f6e9a84690a26d83deb4af6f0b9d
jsr029 <jsr059@gmail.com>
2025-01-18 17:29:35 +0100

    v

--- projects-jsr029-frontend/src/baseUrl.js
+++ projects-jsr029-frontend/src/baseUrl.js
@@ -1,6 +1,2 @@
-//export const baseUrl = "http://localhost:5000"
-export const baseUrl = "https://projects-jsr029-backend.vercel.app"
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/main
+export const baseUrl = "http://localhost:5000"
+//export const baseUrl = "https://projects-jsr029-backend.vercel.app"
