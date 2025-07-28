55f99d587640e79a12c62c686b70d93befee9259
Jean-Sebastien Rakotonirina <78790620+jsr029@users.noreply.github.com>
2025-07-29 00:14:56 +0200

    Update ResumePage.js

--- projects-jsr029-frontend/src/pages/ResumePage.js
+++ projects-jsr029-frontend/src/pages/ResumePage.js
@@ -8,93 +8,105 @@ const ResumePage = () => {
     return (
         <>
             <Helmet>
-                <title>Resume - Jean-Sébastien Rakotonirina</title>
-                <meta name="description" content="Resume of Jean-Sébastien Rakotonirina, Application Developer." />
-                <meta name="keywords" content="Resume, Jean-Sébastien Rakotonirina, Application Developer, JavaScript, React, Node.js" />
-                <meta property="og:title" content="Resume - Jean-Sébastien Rakotonirina" />
-                <meta property="og:description" content="Resume of Jean-Sébastien Rakotonirina, Application Developer." />
+                <title>CV - Jean-Sébastien Rakotonirina</title>
+                <meta name="description" content="CV de Jean-Sébastien Rakotonirina, Développeur d'Applications." />
+                <meta name="keywords" content="CV, Jean-Sébastien Rakotonirina, Développeur d'Applications, JavaScript, React, Node.js" />
+                <meta property="og:title" content="CV - Jean-Sébastien Rakotonirina" />
+                <meta property="og:description" content="CV de Jean-Sébastien Rakotonirina, Développeur d'Applications." />
                 <meta property="og:type" content="website" />
             </Helmet>
+
             <div className="main-container" style={{ marginTop: "45px" }}>
                 <div className="resume-container">
-                    <header className="resume-header">
+                    <header className="resume-header text-center mb-4">
                         <h1>Jean-Sébastien Rakotonirina</h1>
                         <p>Développeur d'Applications</p>
-                        <p>Email: <a href="mailto:jsr059@gmail.com">jsr059@gmail.com</a> | Phone: <a href="tel:+330665985036">(+33) 06.65.32.50.36</a></p>
+                        <p>
+                            Email : <a href="mailto:jsr059@gmail.com">jsr059@gmail.com</a> | Téléphone : <a href="tel:+33665325036">(+33) 6 65 32 50 36</a>
+                        </p>
                     </header>
+
+                    <section className="resume-section">
+                        <h2>Projets récents</h2>
                         <div className="resume-item">
                             <h3>Créateur de drones</h3>
-                            <p>Pour un pays étranger | janvier 2025 - à ce jour</p>
+                            <p>Projet personnel pour un pays étranger | Janvier 2025 - Aujourd'hui</p>
                             <ul>
-                                <li>Modéliser des drones sous Solidworks</li>
-                                <li>Créer la documentation</li>
-                                <li>Créer un prototype</li>
-                                <li>Prise en charge par les investisseurs du traducteur et des drones, en cours</li>
+                                <li>Modélisation des drones sous Solidworks</li>
+                                <li>Rédaction de la documentation</li>
+                                <li>Création de prototypes</li>
+                                <li>Projet en cours avec investisseurs</li>
                             </ul>
                         </div>
 
-                    <section className="resume-section">
-                        <h2>Experience</h2>
                         <div className="resume-item">
-                            <h3>Créateur d'un traducteur de Braille</h3>
-                            <p>Pour moi | janvier 2025 - avril 2025</p>
+                            <h3>Créateur d’un traducteur de Braille</h3>
+                            <p>Projet personnel | Janvier 2025 - Avril 2025</p>
                             <ul>
-                                <li>Modéliser sous Solidworks</li>
-                                <li>Connecter les différents composants</li>
-                                <li>Imprimer en 3D avec Chitubox</li>
-                            </ul>
-                        </div>
-                        <div className="resume-item">
-                            <h3>Développeur d'Appications et Dev UX (Bénévolat)</h3>
-                            <p>Pour Benomads.fr | août 2024 - janvier 2025</p>
-                            <ul>
-                                <li>Créer des prototypes figma pour rendre la navigation intuitive</li>
-                                <li>Développer des micro services comme à l'inscription l'application se crée automatiquement</li>
-                                <li>Node, mongo, express, react, docker, github, gitlab, vscode, ubuntu</li>
-                            </ul>
-                        </div>
-                        <div className="resume-item">
-                            <h3>Développeur Filemaker et Filemaker Server (Bénévolat)</h3>
-                            <p>Pour un Consultant | août 2024 - octobre 2024</p>
-                            <ul>
-                                <li>Créer une application Filemaker</li>
-                                <li>Utiliser les APIs de 2 sites web redmine et jira en créant mes propres APIs</li>
-                                <li>Filemaker Pro et Server, Nodejs</li>
+                                <li>Modélisation sous Solidworks</li>
+                                <li>Connexion des composants électroniques</li>
+                                <li>Impression 3D avec Chitubox</li>
                             </ul>
                         </div>
                     </section>
 
                     <section className="resume-section">
-                        <h2>Education</h2>
+                        <h2>Expérience professionnelle</h2>
+
                         <div className="resume-item">
-                            <h3>Bac+3/4</h3>
+                            <h3>Développeur d’Applications & UX (Bénévolat)</h3>
+                            <p>Benomads.fr | Août 2024 - Janvier 2025</p>
+                            <ul>
+                                <li>Création de prototypes Figma pour améliorer l’expérience utilisateur</li>
+                                <li>Développement de micro-services (ex. création automatique de comptes)</li>
+                                <li>Technos : Node.js, MongoDB, Express, React, Docker, GitHub, GitLab, VS Code, Ubuntu</li>
+                            </ul>
+                        </div>
+
+                        <div className="resume-item">
+                            <h3>Développeur FileMaker & FileMaker Server (Bénévolat)</h3>
+                            <p>Consultant indépendant | Août 2024 - Octobre 2024</p>
+                            <ul>
+                                <li>Création d’une application FileMaker personnalisée</li>
+                                <li>Intégration des API Redmine et Jira via des API Node.js maison</li>
+                                <li>Technos : FileMaker Pro, FileMaker Server, Node.js</li>
+                            </ul>
+                        </div>
+                    </section>
+
+                    <section className="resume-section">
+                        <h2>Formation</h2>
+                        <div className="resume-item">
+                            <h3>Bac+3/4 - Développement Web</h3>
                             <p>OpenClassRooms | 2021 - 2022</p>
                         </div>
                     </section>
 
                     <section className="resume-section">
-                        <h2>Skills</h2>
+                        <h2>Compétences</h2>
                         <ul className="skills-list">
                             <li>JavaScript</li>
                             <li>React</li>
                             <li>Node.js</li>
                             <li>Express</li>
-                            <li>Mongo</li>
+                            <li>MongoDB</li>
                             <li>SQL</li>
                         </ul>
                     </section>
 
                     <section className="resume-section">
                         <h2>Contact</h2>
-                        <p>Email: <a href="mailto:jsr059@gmail.com">jsr059@gmail.com</a></p>
-                        <p>LinkedIn: <a href="https://www.linkedin.com/in/rakotonirinajeansebastien" target="_blank" rel="noopener noreferrer">www.linkedin.com/in/rakotonirinajeansebastien</a></p>
-                        <p>GitHub: <a href="https://github.com/jsr029" target="_blank" rel="noopener noreferrer">https://github.com/jsr029</a></p>
+                        <p>Email : <a href="mailto:jsr059@gmail.com">jsr059@gmail.com</a></p>
+                        <p>LinkedIn : <a href="https://www.linkedin.com/in/rakotonirinajeansebastien" target="_blank" rel="noopener noreferrer">linkedin.com/in/rakotonirinajeansebastien</a></p>
+                        <p>GitHub : <a href="https://github.com/jsr029" target="_blank" rel="noopener noreferrer">github.com/jsr029</a></p>
                     </section>
                 </div>
             </div>
+
             <Footer />
         </>
     );
 };
 
 export default ResumePage;
+
