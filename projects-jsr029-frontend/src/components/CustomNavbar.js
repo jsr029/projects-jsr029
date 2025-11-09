f744d379e4b2e60626cc4dc3b7c8fa2985654eea
jsr029 <jsr059@gmail.com>
2025-11-09 16:00:18 +0100

    responsive navbar

--- projects-jsr029-frontend/src/components/CustomNavbar.js
+++ projects-jsr029-frontend/src/components/CustomNavbar.js
@@ -1,3 +1,4 @@
+// src/components/CustomNavbar.js
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
@@ -9,11 +10,15 @@ const CustomNavbar = ({ setShowLogin, setShowRegister }) => {
   const dispatch = useDispatch();
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    <Navbar bg="dark" variant="dark" expand={false} fixed="top">
       <Container>
         <Navbar.Brand as={Link} to="/">JSR029</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
+        
+        {/* Burger toujours visible */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        {/* Menu qui apparaît dans le burger sur TOUS les écrans */}
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Accueil</Nav.Link>
             <Nav.Link as={Link} to="/cv">CV</Nav.Link>
@@ -21,7 +26,7 @@ const CustomNavbar = ({ setShowLogin, setShowRegister }) => {
           <Nav>
             {isAuthenticated ? (
               <>
-                <Nav.Link disabled>{user?.role}</Nav.Link>
+                <Nav.Link disabled>{user?.role || 'Admin'}</Nav.Link>
                 <Nav.Link onClick={() => dispatch(logout())}>Déconnexion</Nav.Link>
               </>
             ) : (
